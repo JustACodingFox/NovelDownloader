58cd48b3fd75a3a9baacd42a9a653241bcd3e285
Fabian <fabianfuchs1999@googlemail.com>
2019-08-08 02:51:50 -0400

    added spider for webnovel

--- noveldownloader/noveldownloader/spiders/webnovel_spider.py
+++ noveldownloader/noveldownloader/spiders/webnovel_spider.py
@@ -1,11 +1,32 @@
+from string import Template
+
 import scrapy
 
 from data.hosts import Hosts
 from ..items import Chapter
 
 
+def extract_ids(meta_data):
+    book_id, next_chapter_id = None, None
+    for line in meta_data.split('\n'):
+        line = line.strip()
+        if line.startswith(WebnovelSpider.attr_next_chapter_id):
+            parts = line.split(' ')
+            next_chapter_id = parts[2]
+            next_chapter_id = next_chapter_id[1:-2]
+        if line.startswith(WebnovelSpider.attr_book_id):
+            parts = line.split(' ')
+            book_id = parts[2]
+            book_id = book_id[1:-2]
+        if book_id and next_chapter_id:
+            break
+    return book_id, next_chapter_id
+
+
 class WebnovelSpider(scrapy.Spider):
     name = Hosts.WEBNOVEL.value
+    attr_book_id = 'g_data.bookId'
+    attr_next_chapter_id = 'g_data.nextcId'
 
     def __init__(self, *args, **kwargs):
         super(WebnovelSpider, self).__init__(*args, **kwargs)
@@ -16,18 +37,19 @@ class WebnovelSpider(scrapy.Spider):
         else:
             self.start_urls = []
 
+        # set template url
+        self.url_template = Template('https://www.webnovel.com/book/$bookid/$chapterid')
+
         # set name
         if kwargs.get('name'):
             self.book_name = kwargs.get('name')
         else:
             self.book_name = 'unknown'
-        # set beautify
-        if kwargs.get('beautify'):
-            self.beautify = kwargs.get('beautify')
-        else:
-            self.beautify = False
 
     def parse(self, response):
+        meta_info = response.xpath('/html/body/script[7]/text()').get()
+        book_id, next_chapter_id = extract_ids(meta_info)
+
         # get title of the chapter
         title = response.xpath('//h3/text()').get()
 
@@ -38,10 +60,11 @@ class WebnovelSpider(scrapy.Spider):
         item = Chapter()
         item['title'] = title
         item['content'] = content
-        yield item
-        #
-        # # follow next page
-        # next_page = response.xpath('//li[@class="next"]/a/@href').get()
-        #
-        # if next_page is not None:
-        #     yield response.follow(next_page, callback=self.parse)
+        if title and content:
+            yield item
+
+        # follow next page
+        next_page = self.url_template.substitute(bookid=book_id, chapterid=next_chapter_id)
+
+        if int(book_id) and int(next_chapter_id) > -1:
+            yield response.follow(next_page, callback=self.parse)
